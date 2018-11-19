e56e9228a24f6044cefc8628cc80f6ccb568a438
Cherry0202 <ex00ia2432@gmail.com>
2018-11-19 10:37:21 +0900

    ver1.01

---
+++ kadai03/wa21_kadai03/main.js
@@ -0,0 +1,97 @@
+ // let str1 = prompt('文字列を入力してください');
+ // let cnt =0;
+
+    // if(str1 == "11011001") {
+    //     len = str1.length;
+    //     arr = str1.split("");
+    //     // document.write(arr);
+    //     for(let i=0;i<len;i++) {
+    //         if((arr === 1)&&(i<len)){
+    //             cnt++;
+    //             document.write (
+    //                 `${cnt}`
+    //             );
+    //         }
+    //     }
+    // }
+//問１
+//     if(str1 !== null) {
+//         len = str1.length;
+//         arr = str1.split("");
+//         // document.write(arr);
+//           for(let i=0;i<len;i++) {
+//               if((i%2 === 1)&&(i<len)){
+//                   console.log (
+//                      arr[i]
+//                  );
+//               }
+//          }
+//     }
+
+//問３
+    // 11011001
+    // if (str1 === "11011001") {
+    //     let str = str1;
+    //     let count = 0;  // 文字数を格納
+    //
+    //     let ex = /1/g;  // 正規表現
+    //
+    //     let arr = str.match(ex); //配列に格納
+    //
+    //     let len = arr.length;
+    //     // } else {
+    //     //     count = arr.length;
+    //     // }
+    //     for(let i=0;i<len;i++){
+    //         if(i<len){
+    //             cnt++;
+    //         }
+    //     }
+
+    // for(let i=0;i<str1.length;i++){
+    //     if(str1.charAt(i) === "1") {
+    //         cnt++;
+    //     }
+    // }
+    // console.log(
+    //         `${str1}の中の１の数は${cnt}\n`    //カウント数
+    //     );
+    //
+    //}
+//問２
+
+    //もしたもしドッタピオですた。が入力された場合
+//正規表現
+    //  if (str1 === "もしたもしドッタピオですた。") {
+    //     let str2 = str1.replace(/た/g, '');//"た"を空白に
+    //     let str3 = str2.replace(/タ/g, '');//"タ"を空白に
+    //     document.write(
+    //         `${str2}\n`  //配列表示
+    //     );
+    // }
+//ループ
+//     for(let i=0;i<str1.length;i++){
+//         if((str1.charAt(i) === "た")||(str1.charAt(i) === "タ")) {
+//         }else{
+//             console.log(str1[i])
+//         }
+//     }
+//     console.log(
+//         `${str1}の中の１の数は${cnt}\n`    //カウント数
+//     );
+//問４
+//     for(let i=1;i<51;i++){
+//         if(i %3 !== 0){
+//             document.write(
+//                 `${i}<br/>`
+//             );
+//         }
+//
+//         if(i %3 === 0) {
+//             document.write(
+//                 `${i}は３の倍数です<br/>`
+//             );
+//         }
+//     }
+//
+//
